95945f7808cc0660d214841622a70f3056bdc74f
Mohammed Niyas Thottathil (RBEI/ETA22) <Thottathil.MohammedNiyas@in.bosch.com>
2021-05-26 20:23:35 +0530

    Update BMS_Sender.cpp

--- BMS_Sender/BMS_Sender.cpp
+++ BMS_Sender/BMS_Sender.cpp
@@ -1,8 +1,50 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+
+class BMS_parameters
+{
+	int SOC;
+	int temeperature;
+
+public:
+	void generate_parameters()
+	{
+		SOC = rand() % 100 + 1;
+		temeperature = rand() % 100 + 1;
+	};
+
+	int get_temperature()
+	{
+		return temeperature;
+	}
+
+	int get_soc()
+	{
+		return SOC;
+	}
+
+};
+
+class Data_streamer
+{
+	BMS_parameters parameters;
+
+public:
+
+	void stream_console()
+	{
+		parameters.generate_parameters();
+		printf("soc: %d, temperature: %d \n", parameters.get_soc(), parameters.get_temperature());
+	}
+
+};
 
 int main()
 {
-	printf("Hello World\n");
+	Data_streamer streamer;
+
+	streamer.stream_console();
+
 
 	return 0;
 }
