0f8fa999f5663ac12850d6fb1a26e41be442ea2b
PREETI <preetikadyan634@gmail.com>
2021-06-17 17:27:16 +0530

    Add files via upload

---
+++ sender_output_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 12:19:57 2021
+
+@author: EPA5COB
+"""
+
+import json
+import random
+
+for i in range(10):
+    print(json.dumps({"Temperature": random.randint(10,100),"SOC": random.randint(10,100)}))
+    
